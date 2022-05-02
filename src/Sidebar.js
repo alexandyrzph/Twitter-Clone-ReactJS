f96a0455537dcbcd965cfbcfe252b2edb7702016
alexandyrzph <alexandyrzph@gmail.com>
2022-05-02 09:54:32 +0300

    Working on sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SearchIcon from "@mui/icons-material/Search";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Button } from "@mui/material";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter icon */}
+      <TwitterIcon />
+
+      <SidebarOption active Icon={HomeIcon} text="Home" />
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="Message" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>Tweet</Button>
+      {/* Button -> Tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
